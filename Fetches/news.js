26d15d572bb9d9e9c6186835c4e540e90e0e1ba2
Alin Draganescu <alindraganescu@Alins-Mac.local>
2023-08-28 11:08:35 +0200

    modified pagination for news

--- Fetches/news.js
+++ Fetches/news.js
@@ -2,7 +2,9 @@ const axios = require('axios');
 
 const newsData = async (page = 0) => {
   let response = await axios.get(
-    `https://newsdata.io/api/1/news?apikey=${process.env.NewsApiKey}&language=en&q=cryptocurrency&page=${page}`
+    // `https://newsdata.io/api/1/news?apikey=${process.env.NewsApiKey}&language=en&q=cryptocurrency&page=${page}`
+    // The crypto news is not acccessible with the free API key anymore, so we are getting the general news instead
+    `https://newsdata.io/api/1/news?apikey=${process.env.NewsApiKey}&language=en&q=cryptocurrency`
   );
   return response.data;
 };
